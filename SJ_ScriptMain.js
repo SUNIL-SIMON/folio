ea2670c04e1f55056b97005fa523c4c6c0608707
SUNIL-SIMON <78574541+SUNIL-SIMON@users.noreply.github.com>
2021-02-18 11:55:45 +0530

    socio medias

--- SJ_ScriptMain.js
+++ SJ_ScriptMain.js
@@ -107,8 +107,8 @@ function resizeSocialButtons(width)
 {
 
  if(width < 500){
-	document.getElementById("social5").style.fontSize = "10px";
-        var wdt1 =  (width * 0.025) + "px"
+	document.getElementById("social5").style.fontSize = "8px";
+        var wdt1 =  (width * 0.015) + "px"
         document.getElementById("social1").style.width = wdt1;
         document.getElementById("social1").style.height = wdt1;
 	document.getElementById("social2").style.width = wdt1;
@@ -119,8 +119,8 @@ function resizeSocialButtons(width)
         document.getElementById("social4").style.height = wdt1;
   }
   else if(width < 1000){
-	document.getElementById("social5").style.fontSize = "13px";
-        var wdt1 =  (width * 0.025) + "px"
+	document.getElementById("social5").style.fontSize = "10px";
+        var wdt1 =  (width * 0.015) + "px"
         document.getElementById("social1").style.width = wdt1;
         document.getElementById("social1").style.height = wdt1;
 	document.getElementById("social2").style.width = wdt1;
@@ -132,8 +132,8 @@ function resizeSocialButtons(width)
   }
   else
   {
-	document.getElementById("social5").style.fontSize = "18px";
-	var wdt1 =  (width * 0.025) + "px"
+	document.getElementById("social5").style.fontSize = "13px";
+	var wdt1 =  (width * 0.015) + "px"
         document.getElementById("social1").style.width = wdt1;
         document.getElementById("social1").style.height = wdt1;
 	document.getElementById("social2").style.width = wdt1;
