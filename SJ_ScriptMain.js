39955c5eb9efff46541d9ab2dafcd837db067a32
SUNIL-SIMON <78574541+SUNIL-SIMON@users.noreply.github.com>
2021-02-22 11:37:36 +0530

    new view

--- SJ_ScriptMain.js
+++ SJ_ScriptMain.js
@@ -20,36 +20,50 @@ var srcArrayT3 =["SI_td1.jpg","SI_td2.jpg","SI_td1.jpg","SI_td2.jpg","SI_td1.jpg
 document.getElementById("defaultOpen").click();
 
 resizedWindow();
-restoreOpacity();
-restoreColors();
+
 function resizedWindow() {
+
   var w = window.outerWidth;
   var h = window.outerHeight;
 
   resizeFUN_TECH_views(w);
-  resizeImageFlyers(w);
   resizeBottomCards(w);
   resizeTabs(w);
-  resizeSocialButtons(w);
+
   var wdt7 = ((w * 0.14) * 3) + "px";
   var wdt8 = ((w * 0.14) * 2.3) + "px";
- document.getElementById("about_content1").style.height = wdt7;
- document.getElementById("fun").style.height = wdt8;
- document.getElementById("techi").style.height = wdt8;
-}
-function openTab(evt, tabName) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  
+  document.getElementById("content1").style.height = wdt7;
+  document.getElementById("aboutC1").style.height = wdt7;
 
+ 
+  
+}
+function openTab2(evt) {
+
+    document.getElementById("content1").style.display = "block";
+    document.getElementById("content2_1").style.display = "block";
+    document.getElementById("content2_2").style.display = "block";
+    document.getElementById("content3").style.display = "block";
+    document.getElementById("toptab2").style.display = "none";
+    document.getElementById("toptab1").style.display = "block";
+    openTab(evt);
+}
+function openTab1(evt) {
+
+    document.getElementById("content1").style.display = "none";
+    document.getElementById("content2_1").style.display = "none";
+    document.getElementById("content2_2").style.display = "none";
+    document.getElementById("content3").style.display = "none";
+    document.getElementById("toptab2").style.display = "block";
+    document.getElementById("toptab1").style.display = "none";
+    openTab(evt);
+}
+function openTab(evt) {
+  var i, tablinks;
   tablinks = document.getElementsByClassName("tab button");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  document.getElementById(tabName).style.display = "block";
   evt.currentTarget.className += " active";
 }
 function resizeFUN_TECH_views(width) {
@@ -57,30 +71,25 @@ function resizeFUN_TECH_views(width) {
   document.getElementById("fun").style.fontSize = wdt;
   document.getElementById("techi").style.fontSize = wdt;
 }
-function resizeImageFlyers(width)
+function resizeTabs(width)
 {
-  var wdt =  (width * 0.14) + "px";
-
-  document.getElementById("floatingbtn1").style.width = wdt;
-  document.getElementById("floatingbtn1").style.height = wdt;
-  document.getElementById("floatingbtn2").style.width = wdt;
-  document.getElementById("floatingbtn2").style.height = wdt;
-  document.getElementById("floatingbtn3").style.width = wdt;
-  document.getElementById("floatingbtn3").style.height = wdt;
-  document.getElementById("floatingbtn4").style.width = wdt;
-  document.getElementById("floatingbtn4").style.height = wdt;
-  document.getElementById("floatingbtn5").style.width = wdt;
-  document.getElementById("floatingbtn5").style.height = wdt;
-  document.getElementById("floatingbtn6").style.width = wdt;
-  document.getElementById("floatingbtn6").style.height = wdt;
-  document.getElementById("image").style.width = wdt;
-  document.getElementById("image").style.height = wdt;
-
-  var wdt2 =  (width * 0.08) + "px" + " " + "0px";
-  document.getElementById("aboutC1").style.padding = wdt2;
-  var wdt3 =  (width * 0.09) + "px"
-  document.getElementById("imagecontainer").style.top = wdt3;
+  if(width < 500){
+	document.getElementById("defaultOpen").style.fontSize = "13px";
+	document.getElementById("tab2").style.fontSize = "13px";
+  }
+  else if(width < 1000){
+	document.getElementById("defaultOpen").style.fontSize = "15px";
+	document.getElementById("tab2").style.fontSize = "15px";
+  }
+  else
+  {
+	document.getElementById("defaultOpen").style.fontSize = "20px";
+	document.getElementById("tab2").style.fontSize = "20px";
+  }
 }
+
+
+
 function resizeBottomCards(width)
 {
   var wdt =  (width * 0.34) + "px"
@@ -100,313 +109,7 @@ function resizeBottomCards(width)
   document.getElementById("floatingbtn4st2").style.fontSize = wdt2;
   document.getElementById("floatingbtn4st1").style.fontSize = wdt2;
 }
-function resizeTabs(width)
-{
-  if(width < 500){
-	document.getElementById("defaultOpen").style.fontSize = "13px";
-	document.getElementById("tab2").style.fontSize = "13px";
-  }
-  else if(width < 1000){
-	document.getElementById("defaultOpen").style.fontSize = "15px";
-	document.getElementById("tab2").style.fontSize = "15px";
-  }
-  else
-  {
-	document.getElementById("defaultOpen").style.fontSize = "20px";
-	document.getElementById("tab2").style.fontSize = "20px";
-  }
-}
-function resizeSocialButtons(width)
-{
 
- 	var wdt2 =  ((width * 0.80) * 0.10) + "px"
-        document.getElementById("socialmeds").style.height = wdt2;
-        document.getElementById("social1").style.height = wdt2;
-        document.getElementById("social2").style.height = wdt2;
-        document.getElementById("social3").style.height = wdt2;
-        document.getElementById("social4").style.height = wdt2;
-
-	var wdt3 =  ((width * 0.80) * 0.05) + "px"
-	document.getElementById("social5").style.height = wdt3;
-	document.getElementById("social6").style.height = wdt3;
-	document.getElementById("social7").style.height = wdt3;
-
-  if(width < 500){
-	document.getElementById("drop1").style.fontSize = "4px";
-  }
-  else if(width < 1000){
-	document.getElementById("drop1").style.fontSize = "10px";
-  }
-  else
-  {
-	document.getElementById("drop1").style.fontSize = "16px";
-  }
-}
-function restoreOpacity(){
-  document.getElementById("techi").style.opacity = 1;
-  document.getElementById("fun").style.opacity = 1;
-
-  document.getElementById("floatingbtn1").style.opacity = 0;
-  document.getElementById("floatingbtn2").style.opacity = 0;
-  document.getElementById("floatingbtn3").style.opacity = 0;
-  document.getElementById("floatingbtn4").style.opacity = 0;
-  document.getElementById("floatingbtn5").style.opacity = 0;
-  document.getElementById("floatingbtn6").style.opacity = 0;
-  document.getElementById("image").style.opacity = 0;
-}
-function restoreColors(){
- document.getElementById("about_content1").style.backgroundImage = "linear-gradient(to right, #000000 40%,#000000cc)";
-}
-
-
-
-
-
-function moveTechiFunction() {
-  manual = 0;
-  moveTechiFunctionManual();
-}
-
-function moveTechiFunctionManual() {
-  var w = window.outerWidth;
-
-  document.getElementById("fun").style.opacity = 0;
-  document.getElementById("floatingbtn1").style.opacity = 1;
-  document.getElementById("floatingbtn2").style.opacity = 1;
-  document.getElementById("floatingbtn4").style.opacity = 1;
-  document.getElementById("floatingbtn5").style.opacity = 1;
-  document.getElementById("floatingbtn3").style.opacity = 1;
-  document.getElementById("floatingbtn6").style.opacity = 1;
-
-  document.getElementById("floatingbtn1").style.transform = "translate(-145%, -50.5%)";
-  document.getElementById("floatingbtn2").style.transform = "translate(-145%, 60%)";
-  document.getElementById("floatingbtn3").style.transform = "translate(-50%, 44.2%)";
-  document.getElementById("floatingbtn4").style.transform = "translate(-145%, -144.2%)";
-  document.getElementById("floatingbtn5").style.transform = "translate(-50%, -160%)";
-
-  var wdt = (w * 0.155) + "px";
-  document.getElementById("floatingbtn3").style.height = wdt;
-  document.getElementById("floatingbtn4").style.height = wdt;
-  var wdt2 = (w * 0.025) + "px";
-  
-  document.getElementById("floatingbtn3").style.borderBottomRightRadius= wdt2;
-  document.getElementById("floatingbtn2").style.borderBottomLeftRadius= wdt2;
-  document.getElementById("floatingbtn2").style.borderTopLeftRadius= "0px";
-  document.getElementById("floatingbtn1").style.borderBottomLeftRadius= wdt2;
-  document.getElementById("floatingbtn4").style.borderTopLeftRadius= wdt2;
-  document.getElementById("floatingbtn5").style.borderTopRightRadius= wdt2;
-  document.getElementById("floatingbtn5").style.borderBottomRightRadius= wdt2;
-
-  document.getElementById("floatingbtn6"). style.borderTopRightRadius = wdt2;
-  document.getElementById("image"). style.borderTopRightRadius = wdt2;
-
-  var wdt3 = (((w * 0.14) * .05)) + "px";
-  document.getElementById("img1").style.left = wdt3;
-  document.getElementById("img2").style.left = wdt3;
-  document.getElementById("img4").style.left = wdt3;
-  document.getElementById("img6").style.left = wdt3;
-  
-  var wdt4 = (((w * 0.14) * 1.85)) + "px";
-  document.getElementById("img1").style.width = wdt4;
-  document.getElementById("img2").style.width = wdt4;
-  document.getElementById("img4").style.width = wdt4;
-  document.getElementById("img6").style.width = wdt4;
-
-  var wdt5 = (((w * 0.14) * 0.75)) + "px";
-  document.getElementById("img1").style.height = wdt5;
-  document.getElementById("img2").style.height = wdt5;
-  document.getElementById("img4").style.height = wdt5;
-  document.getElementById("img6").style.height = wdt5;
-
-  var wdt6 = (((w * 0.14) * .10)) + "px";
-  document.getElementById("img1").style.top = wdt6;
-  document.getElementById("img2").style.top = wdt6;
-  document.getElementById("img4").style.top = wdt6;
-  document.getElementById("img6").style.top = wdt6;
-
-  document.getElementById("img6").style.opacity = "0";
-  document.getElementById("img1").style.opacity = "1";
-  document.getElementById("floatingbtn6").style.zIndex = "1";
-  document.getElementById("floatingbtn1").style.zIndex = "2";
-
- clearInterval(timer2_cal1);
- timer2Open = 0;
-   if(timer1Open == 0){
-    timer1Open = 1;
-    changeTechImages();
-    timer1_cal1 = setInterval(changeTechImages, 1600);
-   }
-
-}
-function changeTechImages()
-{
-  if(cnt1 <= 4){
-    document.getElementById("img1").src = srcArrayT1[cnt1];
-    document.getElementById("img2").src = srcArrayT2[cnt1];
-    document.getElementById("img4").src = srcArrayT3[cnt1];
-    cnt1 = cnt1 + 1;
-  }
-  if(cnt1 > 4){
-    cnt1 = 0;
-  }
-}
-function awayTechiFunction() {
-  clearInterval(timer1_cal1);
-   timer1Open = 0;
-  restorePos();
-}
-
-
-
-function moveFunFunction() {
-  manual = 0;
-  moveFunFunctionManual();
-}
-function moveFunFunctionManual() {
-
-var w = window.outerWidth;
-
-  document.getElementById("techi").style.opacity = 0;
-  document.getElementById("floatingbtn1").style.opacity = 1;
-  document.getElementById("floatingbtn2").style.opacity = 1;
-  document.getElementById("floatingbtn4").style.opacity = 1;
-  document.getElementById("floatingbtn5").style.opacity = 1;
-  document.getElementById("floatingbtn3").style.opacity = 1;
-  document.getElementById("floatingbtn6").style.opacity = 1;
-
-  document.getElementById("floatingbtn1").style.transform = "translate(45.5%, -50%)";
-  document.getElementById("floatingbtn2").style.transform = "translate(-50%, 60%)";
-  document.getElementById("floatingbtn3").style.transform = "translate(45%, 44.5%)";
-  document.getElementById("floatingbtn4").style.transform = "translate(-50%, -144.5%)";
-  document.getElementById("floatingbtn5").style.transform = "translate(45%, -160%)";
-
-  var wdt = (w * 0.155) + "px";
-  document.getElementById("floatingbtn3").style.height = wdt;
-  document.getElementById("floatingbtn4").style.height = wdt;
-  var wdt2 = (w * 0.025) + "px";
-
-  document.getElementById("floatingbtn2").style.borderBottomLeftRadius= wdt2;
-  document.getElementById("floatingbtn3").style.borderBottomRightRadius= wdt2;
-  document.getElementById("floatingbtn1").style.borderTopRightRadius= wdt2;
-  document.getElementById("floatingbtn4").style.borderTopLeftRadius= wdt2;
-  document.getElementById("floatingbtn5").style.borderBottomRightRadius= wdt2;
-  document.getElementById("floatingbtn5").style.borderTopRightRadius= wdt2;
-
-  document.getElementById("floatingbtn6"). style.borderBottomLeftRadius = wdt2;
-  document.getElementById("image"). style.borderBottomLeftRadius = wdt2;
-
-
-
-  var wdt3 = (((w * 0.14) * .05)) + "px";
-  document.getElementById("img1").style.left = wdt3;
-  document.getElementById("img2").style.left = wdt3;
-  document.getElementById("img4").style.left = wdt3;
-  document.getElementById("img6").style.left = wdt3;
-  
-  var wdt4 = (((w * 0.14) * 1.85)) + "px";
-  document.getElementById("img1").style.width = wdt4;
-  document.getElementById("img2").style.width = wdt4;
-  document.getElementById("img4").style.width = wdt4;
-  document.getElementById("img6").style.width = wdt4;
-
-  var wdt5 = (((w * 0.14) * 0.75)) + "px";
-  document.getElementById("img1").style.height = wdt5;
-  document.getElementById("img2").style.height = wdt5;
-  document.getElementById("img4").style.height = wdt5;
-  document.getElementById("img6").style.height = wdt5;
-
-  var wdt6 = (((w * 0.14) * .10)) + "px";
-  document.getElementById("img1").style.top = wdt6;
-  document.getElementById("img2").style.top = wdt6;
-  document.getElementById("img4").style.top = wdt6;
-  document.getElementById("img6").style.top = wdt6;
-
-  document.getElementById("img6").style.opacity = "1";
-  document.getElementById("img1").style.opacity = "0";
-  document.getElementById("floatingbtn6").style.zIndex = "2";
-  document.getElementById("floatingbtn1").style.zIndex = "1";
-
- clearInterval(timer1_cal1);
- timer1Open = 0;
- if(timer2Open == 0){
-    timer2Open = 1;
-    changeFunImages();
-    timer2_cal1 = setInterval(changeFunImages, 1600);
-   }
-}
-function changeFunImages()
-{
-  if(cnt2 <= 2){
-    document.getElementById("img1").src= srcArray[cnt2];
-    cnt2 = cnt2 + 1;
-  }
-  if(cnt2 > 2){
-    cnt2 = 0;
-  }
-}
-function awayFunFunction() {
- document.getElementById("img1").style.left = "-50px";
-  restorePos();
-}
-
-
-
-
-
-
-function restorePos(){
-  document.getElementById("techi").style.opacity = 1;
-  document.getElementById("fun").style.opacity = 1;
-
-  document.getElementById("floatingbtn1").style.opacity = 0;
-  document.getElementById("floatingbtn2").style.opacity = 0;
-  document.getElementById("floatingbtn3").style.opacity = 0;
-  document.getElementById("floatingbtn4").style.opacity = 0;
-  document.getElementById("floatingbtn5").style.opacity = 0;
-  document.getElementById("image").style.opacity = 0;
-  document.getElementById("floatingbtn6").style.opacity = 0;
-
-  document.getElementById("floatingbtn1").style.transform = "translate(-50%, -50%)";
-  document.getElementById("floatingbtn2").style.transform = "translate(-50%, -50%)";
-  document.getElementById("floatingbtn3").style.transform = "translate(-50%, -50%)";
-  document.getElementById("floatingbtn4").style.transform = "translate(-50%, -50%)";
-  document.getElementById("floatingbtn5").style.transform = "translate(-50%, -50%)";
-
-
-  document.getElementById("img1").style.left = "20px";
-  document.getElementById("img2").style.left = "20px";
-  document.getElementById("img4").style.left = "20px";
-  document.getElementById("img6").style.opacity = "0";
-  document.getElementById("img1").style.opacity = "0";
-
-   document.getElementById("floatingbtn2").style.borderBottomLeftRadius= "0px";
-  document.getElementById("floatingbtn2").style.borderTopLeftRadius= "0px";
-  document.getElementById("floatingbtn3").style.borderBottomRightRadius= "0px";
-  document.getElementById("floatingbtn1").style.borderTopRightRadius= "0px";
-  document.getElementById("floatingbtn4").style.borderTopLeftRadius= "0px";
-  document.getElementById("floatingbtn5").style.borderBottomRightRadius= "0px";
-  document.getElementById("floatingbtn5").style.borderTopRightRadius= "0px";
-
-  document.getElementById("floatingbtn6"). style.borderBottomLeftRadius = "0px";
-  document.getElementById("image"). style.borderBottomLeftRadius = "0px";
-
-  document.getElementById("floatingbtn1").style.borderBottomLeftRadius= "0px";
-  document.getElementById("floatingbtn6"). style.borderTopRightRadius = "0px";
-  document.getElementById("image"). style.borderTopRightRadius = "0px";
-
-  document.getElementById("c1").style.backgroundImage = "linear-gradient(to right, #1f1c18 40%,#8e0e00cc)";
-
-
-  clearInterval(timer2_cal1);
-  timer2Open = 0;
-
-  clearInterval(timer1_cal1);
-  timer1Open = 0;
-
-  isON = 0;
- 
-  
-}
 function  openLinkedin()
 {
   window.open("https://www.linkedin.com/in/sunil-simon-9146391b9/", "_blank"); 
@@ -436,33 +139,5 @@ function openWorkSite()
   window.open("https://www.zoho.com", "_blank"); 
 }
 
-function autoProcess1(){
-  if(manual == 1){
-  myVar1 = setTimeout(autoProcess2, 1700);
-  }
-}
-function autoProcess2(){
-  if(manual == 1){
-  timer1Open = 1;
-  moveTechiFunctionManual();
-  myVar2 = setTimeout(autoProcess3, 1700);
-  }
-}
-function autoProcess3(){
-  if(manual == 1){
-  awayTechiFunction();
-  myVar3 = setTimeout(autoProcess4, 1700);
-  }
-}
-function autoProcess4(){
-  if(manual == 1){
-  timer2Open = 1;
-  moveFunFunctionManual();
-  myVar4 = setTimeout(autoProcess5, 1700);
-  }
-}
-function autoProcess5(){
-  if(manual == 1){
-  awayFunFunction();
-  }
-}
+
+
