e09f292d098eb3b6ac9c1dc8f1a3d1f1335502be
Sunil Simon <78574541+SUNIL-SIMON@users.noreply.github.com>
2026-08-25 18:55:48 +1200

    added new images and snowfall effect

---
+++ src/snowfall.ts
@@ -0,0 +1,139 @@
+const worksnowfall: string[] = [
+ "🧩",
+  "🔄",
+  "▶️",
+  "⏯️",
+  "🌐",
+  "☁️",
+  "🧑‍💻",
+  "🖥️",
+  "⌨️",
+  "🖱️",
+
+  "</>",
+  "{}",
+  "[]",
+  "()",
+  "=>",
+  "===",
+  "!=",
+  "&&",
+  "||",
+  "//",
+  "/* */",
+  "#",
+  "$",
+  ">_",
+
+  "if",
+  "else",
+  "for",
+  "while",
+  "return",
+  "const",
+  "let",
+  "var",
+  "class",
+  "function",
+  "async",
+  "await",
+  "import",
+  "export",
+  "new",
+  "null",
+  "true",
+  "false",
+  "API",
+  "JSON",
+  "HTTP",
+  "REST",
+  "SQL",
+  "Git",
+  "CI/CD",
+  "SDK",
+  "npm",
+  "iOS",
+  "Android",
+  "TS",
+  "JS",
+  "Swift",
+  "Kotlin"
+];
+  const playsnowfall: string[] = [
+  "⛑️",
+  "🪖",
+  "🔧",
+  "🛠️",
+  "⚙️",
+  "🔩",
+  "🚴",
+  "🚴‍♂️",
+  "🚴‍♀️",
+  "🚲",
+  "🚵",
+  "🚵‍♂️",
+  "🚵‍♀️",
+  "🏁",
+  "🚩",
+  "🛣️",
+  "⛑️",
+  "🪖",
+  "🔧",
+  "🛠️",
+  "⚙️",
+  "🔩",
+  "🚴",
+  "🚴‍♂️",
+  "🚴‍♀️",
+  "🚲",
+  "🚵",
+  "🚵‍♂️",
+  "🚵‍♀️",
+  "🏁",
+  "🚩",
+  "🛣️"
+];
+
+const workcontainer = document.getElementById("worksnowfall");
+
+worksnowfall.forEach((text) => {
+
+  const element = document.createElement("span");
+
+  element.textContent = text;
+
+  element.classList.add("work-snow-item");
+
+  // Random horizontal position
+  element.style.left = `${Math.random() * 100}%`;
+
+  // Random starting delay
+  element.style.animationDelay = `${Math.random() * 5}s`;
+
+  // Random falling speed
+  element.style.animationDuration =
+    `${3 + Math.random() * 5}s`;
+
+  workcontainer?.appendChild(element);
+});
+const playcontainer = document.getElementById("playsnowfall");
+playsnowfall.forEach((text) => {
+
+  const element = document.createElement("span");
+
+  element.textContent = text;
+
+  element.classList.add("play-snow-item");
+
+  // Random horizontal position
+  element.style.left = `${Math.random() * 100}%`;
+
+  // Random starting delay
+  element.style.animationDelay = `${Math.random() * 5}s`;
+
+  // Random falling speed
+  element.style.animationDuration =
+    `${3 + Math.random() * 5}s`;
+
+  playcontainer?.appendChild(element);
+});
